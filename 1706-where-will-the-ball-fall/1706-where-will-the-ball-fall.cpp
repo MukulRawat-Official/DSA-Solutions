3ba5d2c3013bce3a534112e5c50e7259ff74c41f
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2022-11-03 13:35:15 +0530

    Time: 72 ms (10.12%), Space: 14.3 MB (8.17%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+
+    vector<int> findBall(vector<vector<int>>& arr) {
+        int n = arr.size();
+        int m = arr[0].size();
+        
+        vector<vector<int>>vis(n+1,vector<int>(m,0));
+        
+        iota(vis[0].begin(),vis[0].end(),1);
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m-1;j++)
+                if(arr[i][j] == 1 && arr[i][j+1] == -1){
+                    vis[i][j] = -2;
+                    vis[i][j+1] = -2;
+                    j++;
+                } 
+        }
+ 
+     
+       
+        for(int i = 0;i<n;i++)
+        {
+          for(int j = 0;j<m;j++){
+              // cout<<vis[i][j]<<" ";
+              if(!vis[i][j] || vis[i][j] == -2) continue;
+              int y = j + arr[i][j];
+              if(y>=0 && y<m){
+                 vis[i+1][y] = vis[i][j];
+                  
+                 if(i<n-1 && arr[i+1][y] == 1 && y<m-1 && arr[i+1][y+1] == -1)
+                     vis[i+1][y] = 0;
+                 else if(i<n-1 && arr[i+1][y] == -1 && y>0 && arr[i+1][y-1] == 1)
+                     vis[i+1][y] = 0;
+              }
+          }
+        }
+        
+        // for(int i =0 ;i<=n;i++){
+        //     for(int j = 0;j<m;j++) cout<<vis[i][j]<<" ";
+        //     cout<<endl;
+        // }
+        
+        vector<int>res(m,-1);
+        
+        for(int i = 0;i<m;i++) {
+            if(vis[n][i])
+            res[vis[n][i]-1] = i;
+        }
+        return res;
+        
+    }
+};
