651c4938605125cb1f19d3c22c91f97c556d368b
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2022-11-29 12:19:19 +0530

    Time: 2456 ms (14.21%), Space: 285.2 MB (9.93%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-         int n = nums.size();
+        int n = nums.size();
+        vector<unordered_map<long,long>>dp(n);
         
-        // declare a dp array of unordered_map
-        
-        vector<unordered_map<long, long>> dp(n + 1);
-        
-        // dp[i][diff] will store the no. of subsequence with common difference of diff, till ith index
-        
-        long ans = 0;
-        
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = i - 1; j >= 0; j--)
-            {
-                long diff = (long) nums[i] - long(nums[j]);
-                
-                // update ans
-                
-                ans += dp[j][diff];
-                
-                // update dp[i][diff]
-                
-                dp[i][diff] += dp[j][diff];
-                
-                dp[i][diff]++;
+        int ans = 0;
+        for(int i = 1;i<n;i++){
+            for(int j = i - 1;j>=0;j--){
+               long diff =  (long) nums[j] - (long) nums[i];
+               ans += dp[j][diff];
+                dp[i][diff] += dp[j][diff] + 1;
             }
         }
         
