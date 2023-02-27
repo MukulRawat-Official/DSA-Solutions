8a9a861cb9147bc9c8fa24b9522dd25a5c15ecc1
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-02-27 10:44:56 +0530

    Time: 2 ms (76.55%), Space: 7.5 MB (44.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
-        int dp[21][21][2];
+        int dp[21][21];
         memset(dp,-1,sizeof(dp));
         
-        auto recurr= [&](int i , int j , int t , auto&& recurr)->int{
-          if(i == j) return t * nums[i];
-           if(dp[i][j][t] != -1) return dp[i][j][t];  
-           int first = recurr(i+1,j,1-t,recurr) + (t == 1? 1 : -1) * nums[i];
-           int second = recurr(i,j-1,1-t,recurr) + (t == 1? 1 : -1) * nums[j];
-           int ans = 0;
-           if(t) ans = max(first,second);
-           else ans = min(first,second);
-            
-           return dp[i][j][t] = ans;
+        auto recurr= [&](int i , int j, auto&& recurr)->int{
+          if(i == j) return nums[i];
+           if(dp[i][j] != -1) return dp[i][j];  
+           int first = nums[i] - recurr(i+1,j,recurr);
+           int second = nums[j] - recurr(i,j-1,recurr);
+    
+           return dp[i][j] = max(first,second);
         };
         
-        return recurr(0,nums.size()-1,1,recurr) >= 0;
+        return recurr(0,nums.size()-1,recurr) >= 0;
     }
 };
