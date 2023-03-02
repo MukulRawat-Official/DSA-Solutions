f6e2911697eec93dd5f8f357128d08acf9ba4ccd
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-03-02 10:59:20 +0530

    Time: 223 ms (48.33%), Space: 137.8 MB (11.96%) - LeetHub

---
+++ 0464-can-i-win/0464-can-i-win.cpp
@@ -0,0 +1,30 @@
+typedef int ll;
+class Solution {
+public:
+    
+    bool canIWin(int mx, int res) {
+    
+    long mxm = mx * (mx+1) / 2;
+    if(mxm<res) return false;
+    vector<vector<ll>>dp(2,vector<ll>( (1<<mx) +1,-1));
+    
+    auto chk = [&](ll t , ll res , ll mask , auto&& chk)->bool{
+        if(dp[t][mask] != -1) return dp[t][mask];
+     
+        for(int i = 1;i<=mx;i++){
+           if( mask &  1<<(i-1))  continue;
+           mask |= 1<<(i-1);
+           if(res - i <= 0 || (chk(1^t , res - i  , mask , chk) == false) )
+               return dp[t][mask ^ (1<<(i-1))] = true;
+           
+           mask ^= 1 <<(i-1);
+        }
+        
+        
+        return dp[t][mask] = false;
+    };
+        
+        
+    return chk(1,res,0,chk);
+    }
+};
