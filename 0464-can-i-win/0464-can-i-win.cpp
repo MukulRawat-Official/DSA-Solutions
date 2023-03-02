736d43252e1d95cabe50b225ff586b3b8ec2df4e
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-03-02 11:10:49 +0530

    Time: 236 ms (47.37%), Space: 137.7 MB (14.12%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.cpp
+++ 0464-can-i-win/0464-can-i-win.cpp
@@ -7,6 +7,7 @@ public:
     long mxm = mx * (mx+1) / 2;
     if(mxm<res) return false;
     vector<vector<ll>>dp(2,vector<ll>( (1<<mx) +1,-1));
+    // turn can be replace by resultant value as we have mask to taken values.
     
     auto chk = [&](ll t , ll res , ll mask , auto&& chk)->bool{
         if(dp[t][mask] != -1) return dp[t][mask];
@@ -14,7 +15,7 @@ public:
         for(int i = 1;i<=mx;i++){
            if( mask &  1<<(i-1))  continue;
            mask |= 1<<(i-1);
-           if(res - i <= 0 || (chk(1^t , res - i  , mask , chk) == false) )
+           if(res - i <= 0 || (chk(1^t , res - i  , mask , chk) == false) ) // need curr true and next false
                return dp[t][mask ^ (1<<(i-1))] = true;
            
            mask ^= 1 <<(i-1);
