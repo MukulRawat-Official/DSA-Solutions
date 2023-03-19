a2175525d3952b8844e5c2ca8cf2824d998a1471
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-03-19 22:36:58 +0530

    Time: 1026 ms (75.03%), Space: 558.4 MB (67.76%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,49 @@
+class WordDictionary {
+public:
+    struct trie{
+       trie* arr[26];
+       bool is_end = false;
+    };
+    trie* head;
+    WordDictionary() {
+        head = new trie();
+    }
+    
+    void addWord(string word) {
+        trie* root = head;
+        
+        for(auto& x : word){
+            if(!root->arr[x-'a']) root->arr[x-'a'] = new trie();
+            root = root->arr[x-'a']; 
+        }
+        root->is_end = true;
+    }
+    
+    bool search(string word) {
+        trie* tmp = head;
+        auto give = [&](trie* tmp , int idx , auto&& give)->bool{
+          if(idx == word.size()) return tmp->is_end;
+          
+          bool ans = false;
+          if(word[idx] == '.'){
+              for(int i = 0;i<26;i++) {
+                if(tmp->arr[i]) ans |= give(tmp->arr[i] , idx+1 , give);  
+              }
+          }
+            
+          else if(tmp->arr[word[idx] - 'a'])
+           ans |= give(tmp->arr[word[idx]-'a'] , idx+1 , give);
+          
+          return ans;
+        };
+        
+        return give(tmp , 0 , give);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
