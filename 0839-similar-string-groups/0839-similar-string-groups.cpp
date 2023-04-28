5d5a0cebe37fe75b33c4e424afe4539fb5aadcc1
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-04-29 02:45:08 +0530

    Time: 40 ms (72.87%), Space: 10 MB (90.85%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int>par;
+    int find(int a){
+        if(par[a] == a) return a;
+        return par[a] = find(par[a]);
+    }
+    bool join(int a, int b){
+        a = find(a);
+        b = find(b);
+        int t = 0;
+        if(a != b){
+          par[b] = a;
+          t = 1;
+        }
+        
+        return t;
+    }
+    int numSimilarGroups(vector<string>& arr) {
+        int n = arr.size();
+        int m = arr[0].size();
+        par.resize(n);
+        iota(par.begin(),par.end(),0);
+        int comp = n;
+        
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+              int cnt = 0;
+              if(find(i) == find(j)) continue;
+                
+              for(int k = 0;k<m && cnt < 3;k++){
+                   cnt += (arr[i][k] != arr[j][k]);    
+              }  
+                
+              if(cnt % 2 == 0 && join(i,j)) comp--;
+            }
+        }
+        
+        return comp;
+    }
+};
