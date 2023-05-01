5e872ebdff23a6bb72f391703de1b14b60d45376
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-05-01 20:42:43 +0530

    Time: 963 ms (12.51%), Space: 139.6 MB (68.75%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,84 +1,41 @@
-class disjointSet{
-    vector<int>parent,size;
-    public:
-        disjointSet(int n){
-            parent.resize(n+1);
-            size.resize(n+1,1);
-            for(int i=0; i<=n; i++){
-                parent[i]=i;
-            }
-        }
-        int findParent(int node){
-            if(node==parent[node]){
-                return node;
-            }
-            return parent[node]=findParent(parent[node]);
-        }
-        void unionBySize(int u,int v){
-            int parentOfU=findParent(u);
-            int parentOfV=findParent(v);
-            if(parentOfU==parentOfV) return;
-            if(size[parentOfU] < size[parentOfV]){
-                parent[parentOfU]=parentOfV;
-                size[parentOfV]+=size[parentOfU];
-            }
-            else{
-                parent[parentOfV]=parentOfU;
-                size[parentOfU]+=size[parentOfV];
-            }
-        }
-        bool check(int first,int second){
-            if(findParent(first)==findParent(second)) 
-                return true;
-            return false;
-        }
-};
-
 class Solution {
 public:
+    struct DSU{
+        vector<int>par;
+        int cmp;
+        
+        DSU(int n){
+          par.resize(n+1,1);
+          iota(par.begin(),par.end(),0);
+          cmp = n;
+        }
+        
+        int find(int a){ return par[a] == a ? a : par[a] = find(par[a]);}
+        bool join(int a, int b){
+           a = find(a);
+           b = find(b);
+           if(a == b) return false;
+           par[b] = a;
+           cmp--;
+           return true;
+        }
+    };
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        disjointSet ds1(n);
-        disjointSet ds2(n);
-        int alice=0,bob=0,both=0;
-        for(auto it:edges){
-            int type=it[0];
-            int node1=it[1];
-            int node2=it[2];
-            if(type==1 || type==2)continue;
-            if(ds1.check(node1,node2) && ds2.check(node1,node2)){
-                both++;
-            }
-            else{
-                ds1.unionBySize(node1,node2);
-                ds2.unionBySize(node1,node2);
-            }
-        }
-        for(auto it:edges){
-            int type=it[0];
-            int node1=it[1];
-            int node2=it[2];
-            if(type==1){
-                if(ds1.check(node1,node2))alice++;
-                else ds1.unionBySize(node1,node2);
-            }
-            else if(type==2){
-                if(ds2.check(node1,node2))bob++;
-                else ds2.unionBySize(node1,node2);
-            }
-        }
-        unordered_set<int>st1;
-        unordered_set<int>st2;
-        for(int i=1; i<=n; i++){
-            st1.insert(ds1.findParent(i));
-            if(st1.size()>1)break;
-        }
-        for(int i=1; i<=n; i++){
-            st2.insert(ds2.findParent(i));
-            if(st2.size()>1)break;
-        }
-
-        if(st1.size()==1 && st2.size()==1)
-        return alice+bob+both;
+         sort(edges.rbegin(),edges.rend());
+         DSU a(n) , b(n);
+         
+         int ans = 0;
+         for(auto& x : edges){
+             int t = x[0] , u = x[1] , v = x[2];
+             if(t == 3){
+                 ans += (a.join(u,v) + b.join(u,v)) == 0;
+             }
+             else if(t == 2) ans += !a.join(u,v);
+             else ans += !b.join(u,v);
+         }
+        
+        if(a.cmp == 1 && b.cmp == 1) return ans;
         return -1;
     }
+
 };
