48fba1b37257d6cd4dca12ceffae5b14dc2e322a
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-05-01 21:04:29 +0530

    Time: 646 ms (77.70%), Space: 109.6 MB (86.73%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.cpp
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.cpp
@@ -1,60 +1,58 @@
-class DisjointSet{
-    public:
-    vector<int>size;
-    vector<int>parent;
-    DisjointSet(int n){
-        size.resize(n,1);
-        parent.resize(n);
-        for(int i=0;i<n;i++)parent[i]=i;
-    }
-    
-    int findUpar(int node){
-        if(node==parent[node])return node;
-
-       return parent[node]=findUpar(parent[node]);
-    }
-
-    void UnionBySize(int u,int v){
-        int ulp_u=findUpar(u);
-        int ulp_v=findUpar(v);
-        if(ulp_u==ulp_v)return ;
-        if(size[ulp_u]<size[ulp_v]){
-            parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
-        }
-        else{
-            parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
-        }
-       
-    }
-};
-
 class Solution {
 public:
-    static bool cmp(vector<int>&a,vector<int>&b){
-        return a[2]<b[2];
-    }
-    vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>>& edgeList, vector<vector<int>>& queries) {
-        DisjointSet ds(n);
+    
+    struct DSU{
+      vector<int>par;
+      int cmp;
+      
+      DSU(int n){
+          par.resize(n+1,0);
+          iota(par.begin(),par.end(),0);
+          cmp = n;
+      }
+      
+      int find(int a){
+          return a == par[a] ? a : par[a] = find(par[a]);
+      } 
         
-        for(int i=0;i<queries.size();i++){
-            queries[i].push_back(i);
-        }
-        vector<bool>ans(queries.size(),false);
-        sort(queries.begin(),queries.end(),cmp);
-        sort(edgeList.begin(),edgeList.end(),cmp);
-        int j=0;
-        for(int i=0;i<queries.size();i++){
-            while(j<edgeList.size()&&edgeList[j][2]<queries[i][2]){
-                ds.UnionBySize(edgeList[j][0],edgeList[j][1]);
-                j++;
+      bool join(int a, int b){
+          a = find(a);
+          b = find(b);
+          
+          if(a == b) return false;
+    
+          par[a] = b;
+          cmp--;
+          return true;
+      }
+    };
+    vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>>& edgeList, vector<vector<int>>& queries) {
+        DSU ds(n);
+        sort(edgeList.begin(),edgeList.end(), [&](vector<int>& a , vector<int>&b){
+          return a[2] < b[2]; 
+        });
+        
+        int cnt = 0;
+        for(auto& x : queries) x.push_back(cnt++);
+        sort(queries.begin(),queries.end(),[&](vector<int>& a , vector<int>&b){
+           return a[2] < b[2]; 
+        });
+        vector<bool>ans(queries.size(),0);
+        
+        cnt = 0;
+        for(auto& x : queries){
+            int u,v;
+            while(cnt < edgeList.size() && x[2] > edgeList[cnt][2]){
+                u = edgeList[cnt][0];
+                v = edgeList[cnt][1];
+                ds.join(u,v); cnt++;
             }
-            if(ds.findUpar(queries[i][0])==ds.findUpar(queries[i][1])){
-                ans[queries[i][3]]=true;
-            }
+            
+            u = x[0]; v = x[1];
+            ans[x[3]] = ds.find(u) == ds.find(v);
         }
+        
         return ans;
-
+        
     }
 };
