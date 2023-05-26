18666087c528f90fa03f8a8f74c69cc2cd23d95b
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-05-26 17:22:57 +0530

    Time: 3 ms (82.54%), Space: 9.1 MB (77.78%) - LeetHub

---
+++ 0837-new-21-game/0837-new-21-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+       int sz = min(n + 1 , k + maxPts);
+       vector<double>dp(sz);
+       for(int i = k;i<sz;i++) dp[i] = 1;
+       if(k){
+           dp[k-1] = (sz - k) * 1. / maxPts;
+       }
+       for(int i = k-2;i>=0;i--){
+           // dp[i] = dp[i+1] + .... dp[i + mx]
+           // dp[i+1] = dp[i+2] .... dp[i + mx + 1]
+           // dp[i] = dp[i+1] + (dp[i+1] - dp[i + mx + 1]) / maxPts;
+           double curr = dp[i+1];
+           if(i + 1 + maxPts < sz) curr -= dp[i+1+maxPts];
+           dp[i] = dp[i+1] + curr / maxPts;
+       }
+        
+       return dp[0];
+        
+    }
+};
