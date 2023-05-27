9c3a0b980aaf1143c2ef6a11c8b1ce2903887898
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-05-27 23:57:39 +0530

    Time: 269 ms (57.82%), Space: 132.8 MB (57.11%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -7,14 +7,12 @@ public:
         auto recurr = [&](int i ,auto&& recurr)->int{
            if(i == n) return 0;
            if(dp[i] != -1) return dp[i];
-           
-           long ans = INT_MIN;
-            
+           long ans = INT_MIN;       
            long sum = 0;
            for(int j = 0;j<3 && j + i < n;j++)
            {
                 sum += arr[j+i];
-                ans = max(ans , - recurr(j+i+1,recurr) + sum);
+                ans = max(ans , sum - recurr(j+i+1,recurr));
            }        
            return dp[i] = ans;
         };
