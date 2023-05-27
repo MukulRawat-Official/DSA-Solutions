55771c4dde1eb79dc9099dc4ac5a0de99378c056
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-05-27 23:57:12 +0530

    Time: 262 ms (63.74%), Space: 132.9 MB (57.11%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& arr) {
         int n = arr.size();
-        int dp[n+1][2];
+        int dp[n+1];
         memset(dp,-1,sizeof(dp));
-        auto recurr = [&](int i , int t,auto&& recurr)->int{
+        auto recurr = [&](int i ,auto&& recurr)->int{
            if(i == n) return 0;
-            if(dp[i][t] != -1) return dp[i][t];
-           long ans = t ? INT_MIN : INT_MAX;
+           if(dp[i] != -1) return dp[i];
+           
+           long ans = INT_MIN;
             
            long sum = 0;
            for(int j = 0;j<3 && j + i < n;j++)
            {
                 sum += arr[j+i];
-                if(t) ans = max(ans , recurr(j+i+1 , t ^ 1,recurr) + sum);
-                else ans = min(ans , recurr(j+i+1 , t ^ 1,recurr) - sum);
-           }
-           
-           return dp[i][t] = ans;
+                ans = max(ans , - recurr(j+i+1,recurr) + sum);
+           }        
+           return dp[i] = ans;
         };
         
-        long res = recurr(0,1,recurr);
+        long res = recurr(0,recurr);
         
         return (res > 0 ? "Alice" : (res < 0 ? "Bob" : "Tie"));
     }
