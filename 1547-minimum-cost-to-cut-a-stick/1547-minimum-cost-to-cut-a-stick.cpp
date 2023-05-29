e98f57ed98d568c8b9559ef0483ae97d1115d86b
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-05-29 11:10:21 +0530

    Time: 68 ms (72.02%), Space: 8.3 MB (99.78%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -5,20 +5,26 @@ public:
         int m = arr.size();
         int dp[m][m];
         memset(dp,-1,sizeof(dp));
-        auto recurr = [&](int l , int r ,int low , int high, auto&& recurr)->int{
+        auto give = [&](int l , int r)->int{
+           pair<int,int>tmp = {0,n};
+           if(l) tmp.first = arr[l-1];
+           if(r + 1 < m) tmp.second = arr[r+1];
+           return tmp.second - tmp.first;
+        };
+        auto recurr = [&](int l , int r, auto&& recurr)->int{
             if(r < l) return 0;  
             
             if(dp[l][r] != -1) return dp[l][r];
             int ans = INT_MAX;
-            int sum = high - low; 
+            int sum = give(l,r); 
             for(int j = l;j<=r;j++){
-                ans = min(ans, recurr(l,j-1,low,arr[j] , recurr) + recurr(j+1,r,arr[j] , high , recurr) + sum);
+                ans = min(ans, recurr(l,j-1,recurr) + recurr(j+1,r,recurr) + sum);
             }
             
             return  dp[l][r] = ans;
             
         };
         
-        return recurr(0,arr.size() - 1, 0 , n , recurr);
+        return recurr(0,arr.size() - 1,recurr);
     }
 };
