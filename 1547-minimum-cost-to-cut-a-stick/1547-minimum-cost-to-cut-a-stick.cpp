b4db80cfdf8184878ca54fd1c4c6f18c8977afe3
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-05-29 11:06:37 +0530

    Time: 89 ms (53.98%), Space: 8.4 MB (96.00%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    
-    int minCost(int n, vector<int>& cuts) {
-        cuts.push_back(0);
-        cuts.push_back(n);
-        int m=cuts.size();
-        sort(cuts.begin(), cuts.end());
-        vector cost(m, vector<int>(m, 0));
-        for (int step=2; step<m; step++){
-            for (int i=0; i<m-step; i++){
-                int result=INT_MAX, j=i+step;
-                for(int k=i+1; k<j; k++)
-                    result=min(result, cost[i][k] +cost[k][j]);
-                cost[i][j] = result+(cuts[j]-cuts[i]);
+    int minCost(int n, vector<int>& arr) {
+        sort(arr.begin() , arr.end());
+        int m = arr.size();
+        int dp[m][m];
+        memset(dp,-1,sizeof(dp));
+        auto recurr = [&](int l , int r ,int low , int high, auto&& recurr)->int{
+            if(r < l) return 0;  
+            
+            if(dp[l][r] != -1) return dp[l][r];
+            int ans = INT_MAX;
+            int sum = high - low; 
+            for(int j = l;j<=r;j++){
+                ans = min(ans, recurr(l,j-1,low,arr[j] , recurr) + recurr(j+1,r,arr[j] , high , recurr) + sum);
             }
-        }
-        return cost[0][m-1];
+            
+            return  dp[l][r] = ans;
+            
+        };
+        
+        return recurr(0,arr.size() - 1, 0 , n , recurr);
     }
 };
