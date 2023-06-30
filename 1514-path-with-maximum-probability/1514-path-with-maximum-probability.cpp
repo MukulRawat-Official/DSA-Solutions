d8aa724f4d5af5c9e3de1e4aa0aa297a1a076ce9
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-06-30 20:46:14 +0530

    Time: 211 ms (58.67%), Space: 66.2 MB (54.41%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        unordered_map<int, vector<pair<int, int>>> graph;
-        
-        // Build the graph using an adjacency list representation
-        // Each edge is represented as a pair (neighbor node, edge index)
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][0], v = edges[i][1], edgeIdx = i;
-            graph[u].push_back({v, edgeIdx});
-            graph[v].push_back({u, edgeIdx});
+        priority_queue<pair<double , int>>pq;
+        vector<vector<pair<int,double>>>adj(n);
+        for(int i = 0;i<edges.size();i++){
+            int u = edges[i][0] , v = edges[i][1];
+            double w = succProb[i];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
         }
-        
-        vector<double> probabilities(n, 0.0);
-        probabilities[start] = 1.0;
-        
-        priority_queue<pair<double, int>> nodesToExplore;
-        // Add the start node to the priority queue with probability 1.0
-        nodesToExplore.push({1.0, start});
-        
-        while (!nodesToExplore.empty()) {
-            auto [prob, u] = nodesToExplore.top();
-            nodesToExplore.pop();
-            
-            // If we've reached the end node, return the probability
-            if (u == end) 
-                return prob;
-            // If we've already explored this node with a higher probability, skip it
-            if (probabilities[u] > prob) 
-                continue;
-                
-            // Explore the neighbors of the current node
-            for (auto [v, edgeIdx] : graph[u]) {
-                double newProb = prob * succProb[edgeIdx];
-                
-                // Update the probability of the neighbor node if we've found a higher probability
-                if (newProb > probabilities[v]) {
-                    probabilities[v] = newProb;
-                    
-                    // Add the neighbor node to the priority queue with its new probability
-                    nodesToExplore.push({newProb, v});
+        double ans = 0;
+        vector<double>vis(n,0); vis[start] = 1;
+        pq.push({1,start});
+        int node;
+        double val;
+        while(pq.size()){
+           tie(val,node) = pq.top(); pq.pop();
+           
+            for(auto [x,vl] : adj[node]){
+                if(x == end)  ans = max(ans , vl * val);    
+                else if(vis[x] < vl * val) {
+                  vis[x] = vl * val;
+                  pq.push({val * vl , x});
                 }
             }
         }
         
-        // If we've explored all nodes and haven't reached the end node, return 0.0
-        return 0.0;
+        
+        return ans;
     }
 };
