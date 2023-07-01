c0c72b1fd32f4ba621bc4c520436e84ecb962400
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-07-01 12:09:58 +0530

    Time: 462 ms (18.40%), Space: 8.6 MB (93.73%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-
-         int n = locations.size();
-         int mod = 1e9 + 7;
-         vector<vector<int>>dp(n,vector<int>(fuel+1));
-
-         dp[start][fuel]=1;
-
-
-         for(int f = fuel;f>=0;f--){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-
-                    int diff = abs(locations[i] - locations[j]);
-
-                    if(i != j && f >= diff)
-                    dp[j][f-diff] = (dp[j][f-diff]%mod + dp[i][f]%mod)%mod;
+        int n = locations.size();
+        int dp[n][fuel+1];
+        memset(dp,0,sizeof(dp));
+        int mod = 1e9 + 7;
+        dp[start][fuel] = 1;
+        for(int w = fuel;w>=0;w--){
+            for(int i = 0;i<n;i++)
+                for(int j = 0;j<n;j++){
+                    int t = abs(locations[i] - locations[j]);
+                    if(i != j && t <= w){
+                        dp[j][w - t] = (dp[j][w-t] + dp[i][w]) % mod;
+                    }
                 }
-            }
-         }
-
-         int ans = 0;
-
-         for(int i=0;i<=fuel;i++)
-          ans = (ans%mod + dp[finish][i]%mod)%mod;
-
-        return ans;   
+        }
+        
+        int ans = 0;
+        for(auto& x: dp[finish]) ans = (ans + x) % mod;
+        return ans % mod;
+        
     }
 };
