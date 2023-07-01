b895683f8ce77b483dfa7ad4f9f5785116bed961
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-07-01 12:42:07 +0530

    Time: 133 ms (36.02%), Space: 8.5 MB (69.72%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,37 +1,22 @@
+int dp[20][10005];
 class Solution {
 public:
-    int dp[20][10001];
-    int solve(int idx, int dif, vector<int> &rods)
-    {
-        if(idx == rods.size())
-        {
-            if(dif == 0)
-            {
-                return 0;
-            }
-
-            return -1e5;
-        }
-
-        if(dp[idx][dif + 5000] != -1)
-        {
-            return dp[idx][dif + 5000];
-        }
-
-        // dif = num1 - nums2
-        int ans1 = solve(idx+1, dif, rods);  
-
-        // (nums1 + rods[idx]) - nums2 = (nums1 - nums2) + rods[idx]   = d + rods[idx]
-        int ans2 = rods[idx] + solve(idx+1, dif + rods[idx], rods); 
-
-        // nums1 - (nums2 + rods[idx]) = (nums1 - nums2) - rods[idx] = d - rods[idx]
-        int ans3 = rods[idx] + solve(idx+1, dif - rods[idx], rods);  
-
-        return dp[idx][dif + 5000] = max(ans1, max(ans2, ans3));
-    }
     int tallestBillboard(vector<int>& rods) {
-        memset(dp, -1, sizeof(dp));
-        int ans = solve(0,0,rods)/2; 
-        return ans;
+        
+        int n = rods.size();
+        for(int i= 0;i<n;i++) for(auto& x : dp[i]) x = -1;
+        function<int(int,int)> recurr = [&](int i , int w){
+           if(i == n)    return !w ?  0 : -(int)1e6;
+            
+           int& ans = dp[i][w + 5000]; if(ans != -1) return ans; 
+           ans = recurr(i + 1 , w); 
+           ans = max(ans , recurr(i + 1, w - rods[i]));
+           ans = max(ans , recurr(i + 1, w + rods[i]) + rods[i]);
+        
+           return ans;
+        };
+        
+        return max(0 ,recurr(0,0));
+        
     }
 };
