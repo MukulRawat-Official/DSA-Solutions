12c45f8b05a67934877b344f1ad23d4156bb989a
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-07-27 15:31:16 +0530

    Time: 177 ms (75.90%), Space: 55.9 MB (36.33%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    typedef long long ll;
+    long long maxRunTime(int n, vector<int>& arr) {
+        long long low = 0 , high = 1;
+        for(auto& x : arr) high += x;
+        auto chk = [&](ll mid)->bool{
+           ll ctr = n * mid;  
+           ll l = 0 , h = arr.size() - 1;
+           for(auto& x : arr) ctr -= min(mid , 1ll * x);
+           return ctr<1;
+        };
+        while(high - low > 1){
+            ll mid = (high + low) >> 1;
+            
+            if(chk(mid)) low = mid;
+            else high = mid;
+        }
+        
+        return low;
+    }
+};
