d2c7c1953835dcf767f0433e7debca138c779f2f
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-08-02 03:02:35 +0530

    Time: 80 ms (15.05%), Space: 12.7 MB (19.62%) - LeetHub

---
+++ 1434-number-of-ways-to-wear-different-hats-to-each-other/1434-number-of-ways-to-wear-different-hats-to-each-other.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+   
+    int numberWays(vector<vector<int>>& arr) {
+        int n = arr.size();
+        
+        int mx  = (1<<n) - 1;
+        vector<vector<long>>dp(41,vector<long>(mx+1,-1));
+        int mod = 1e9 + 7;
+        
+    
+        function<int(int,int)> recurr = [&](int i , int mask){
+           if(mask == mx) return 1; 
+           if(i >= 40) return 0;
+            
+           long& ans = dp[i][mask];
+           if(ans != -1) return (int)ans;
+           ans = recurr(i+1,mask) % mod;
+            
+           for(int j = 0;j<n;j++){
+               if(mask & (1<<j)) continue;
+               for(auto& y : arr[j]){  
+                  if(y == i + 1){
+                      mask ^= (1<<j);
+                      ans = (ans + recurr(i+1,mask)) % mod;
+                      mask ^= (1<<j);
+                  }
+                   
+               }
+           }
+            
+            
+           return (int)ans;
+        };
+        int mask = 0;
+        return recurr(0,mask);
+    }
+};
