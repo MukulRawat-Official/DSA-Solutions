3c12d7fe1e02b4121cb49f059642e915b05d6485
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-08-17 15:13:28 +0530

    Time: 71 ms (78.43%), Space: 29.1 MB (92.96%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,16 +3,16 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size() , m = mat[0].size();
         vector<vector<int>>ans(n,vector<int>(m,n * m + 1));
-        priority_queue<pair<int,int>>pq;
+        queue<int>q;
         for(int i = 0;i<n;i++) for(int j=0;j<m;j++){
             if(!mat[i][j]){
                 ans[i][j] = 0;
-                pq.push({0,i * m + j});
+                q.push(i * m + j);
             }
         }
         vector<int> l = {0,1,0,-1} , r = {1,0,-1,0};
-        while(pq.size()){
-            int curr = pq.top().second; pq.pop();
+        while(q.size()){
+            int curr = q.front(); q.pop();
             int x = curr / m , y = curr % m;
             for(int k = 0;k<4;k++){
                 int i = x + l[k];
@@ -21,7 +21,7 @@ public:
                 if(i < 0 || j < 0 || i ==n || j == m || ans[i][j] <= ans[x][y] + mat[i][j])
                     continue;
                 ans[i][j] = ans[x][y] + mat[i][j];
-                pq.push({-ans[i][j] , i * m + j});
+                q.push(i * m + j);
             }
         }
         
