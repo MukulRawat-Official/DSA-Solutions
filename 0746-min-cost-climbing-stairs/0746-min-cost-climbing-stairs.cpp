0981aa535d4dfae9cb848209f12b49367a4748e2
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-10-13 23:33:38 +0530

    Time: 0 ms (100.00%), Space: 14 MB (26.23%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     int dp[1001] , n;
-    int recurr(vector<int>&arr, int i)
-    {
-        if(i >= n)
-            return 0;
-        
-        if(dp[i] != -1) return dp[i];
-        
-        dp[i] = INT_MAX;
-        
-        for(int  j =1;j<=2;j++)
-        {
-          dp[i] = min(dp[i] , recurr(arr,i+j) + arr[i]);
-        }
-        
-        return dp[i];
-    }
-    
     int minCostClimbingStairs(vector<int>& cost) {
         n = cost.size();
-        memset(dp,-1,sizeof(dp));
+        cost.push_back(0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
         
-        int x = recurr(cost, 0);
-        memset(dp,-1,sizeof(dp));
+        for(int i = 2;i<=n;i++){
+            dp[i] = min(dp[i-1] , dp[i-2]) + cost[i];
+        }
         
-        x = min(x , recurr(cost , 1));
+        return dp[n];
         
-        return x;
     }
 };
