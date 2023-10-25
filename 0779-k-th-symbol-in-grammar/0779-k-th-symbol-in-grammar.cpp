8bd19b8c3940cd1dc3459039e3bcaa520bdcdcc4
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-10-25 23:49:41 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (12.22%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int cnt = 0 , ans = -1 , k;
+    void recurr(int i , int n , int tot ,  int curr){
+        if(i == n){
+            if(k == 1)    ans = curr;
+            return;
+        }
+          
+        if(  k  > tot / 2 ){
+            k -= tot / 2;
+            recurr(i+1 , n , tot / 2 , !curr);
+        }
+        else{
+            recurr(i+1, n , tot / 2 , curr);
+        }
+    }
+    int kthGrammar(int n, int k) {
+        this->k = k;
+        int tot = pow(2,n - 1);
+        recurr(1,n, tot , 0);
+        
+        return ans;
+    }
+};
