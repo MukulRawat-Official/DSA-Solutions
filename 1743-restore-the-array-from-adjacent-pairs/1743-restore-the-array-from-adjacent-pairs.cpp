d8e6fc138c3af2797b94d9f58cbb6b507e287c15
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-11-10 12:02:40 +0530

    Time: 664 ms (13.94%), Space: 178.4 MB (5.15%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        int n = adjacentPairs.size() + 1;
+        map<int,vector<int>>adj;
+        map<int,int>inc;
+        vector<int> ans;
+        
+        for(auto& x : adjacentPairs){
+            int u = x[0] - 1, v = x[1] - 1;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            inc[u]++;
+            inc[v]++;
+        }
+        
+        
+        
+        
+        
+        
+        function<void(int,int)> dfs = [&](int curr , int prv){
+            ans.push_back(curr + 1);  
+            for(auto&   x  : adj[curr]){
+                if(x == prv) continue;
+                dfs(x,curr);
+            }
+        };
+        
+        for(auto& [x,f] : inc){
+            if(f == 1){
+                dfs(x,-1);
+                break;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
