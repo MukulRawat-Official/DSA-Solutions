c70073f669d2a7448a1ffc8ee0b7f60a68e167a3
Mukul Rawat <101347992+MukulRawat-Official@users.noreply.github.com>
2023-11-14 20:30:09 +0530

    Time: 407 ms (39.73%), Space: 13.4 MB (56.62%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -4,17 +4,16 @@ public:
         int n = s.size();
         vector<int>left(26,0) , right(26,0);
         long long ans = 0;
-        left[s[0] - 'a']++;
         vector<unordered_set<char>>cnt(26);
         for(int i = 1;i<n;i++) right[s[i] - 'a']++;
         for(int i = 1;i<n;i++) {
            right[s[i] - 'a']--;
+           left[s[i-1] - 'a']++;
            for(int j = 0;j<26;j++){
                if(left[j] && right[j]){
                    cnt[j].insert(s[i]);
                }
            }
-           left[s[i] - 'a']++;
         }
         
         for(auto& x : cnt) ans += x.size();
